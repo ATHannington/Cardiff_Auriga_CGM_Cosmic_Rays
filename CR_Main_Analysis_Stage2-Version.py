Fixed treatment of radii in logarithmic units, fixed PDFs, stats, and phase plots binning

CR_Main_Analysis_Stage2-Version.py
@@ -127,7 +127,6 @@ imageCmapDict = {
     "n_HI_col": (CRPARAMSMASTER["colourmapMain"].split("_"))[0],
 }
 
-
 xlimDict = {
     "R": {}, #{"xmin": CRPARAMSMASTER["Rinner"], "xmax": CRPARAMSMASTER["Router"]},
     "mass": {"xmin": 4.0, "xmax": 9.0},
@@ -135,20 +134,19 @@ xlimDict = {
     "T": {"xmin": 3.5, "xmax": 7.0},
     "n_H": {"xmin": -6.0, "xmax": 1.0},
     "n_HI" : {"xmin": -13.0, "xmax": 0.0},
-    "n_H_col": {"xmin": 19.0, "xmax": 21.5},
-    "n_HI_col" : {"xmin": 12.0, "xmax": 21.5},
-    "M_H" : {},
-    "M_HI" : {},
-    "B": {"xmin": -2.5, "xmax": 1.0},
+    "n_H_col": {"xmin": 19.0, "xmax": 22.0},
+    "n_HI_col" : {"xmin": 12.5, "xmax": 22.0},
+    "B": {"xmin": -2.5, "xmax": 2.0},
     "vrad": {"xmin": -200.0, "xmax": 200.0},
     "vrad_in": {"xmin": -200.0, "xmax": 200.0},
     "vrad_out": {"xmin": -200.0, "xmax": 200.0},
     "gz": {"xmin": -2.0, "xmax": 1.0},
+    "Pressure" : {"xmin": -16.0, "xmax": -10.0},
     "P_thermal": {"xmin": -16.0, "xmax": -10.0},
     "P_CR": {"xmin": -19.5, "xmax": -10.0},
     "PCR_Pthermal": {"xmin": -4.5, "xmax": 2.5},
     "PCR_Pmagnetic": {"xmin": -3.5, "xmax": 2.5},
-    "Pthermal_Pmagnetic": {"xmin": -2.0, "xmax": 4.0},
+    "Pthermal_Pmagnetic": {"xmin": -2.5, "xmax": 3.5},
     "P_magnetic": {"xmin": -19.5, "xmax": -10.0},
     "P_kinetic": {"xmin": -19.5, "xmax": -10.0},
     "P_tot": {"xmin": -19.5, "xmax": -10.0},
@@ -168,9 +166,8 @@ xlimDict = {
     "nh" : {"xmin": -7.0, "xmax": 1.0},
     "e_CR": {"xmin": -8.0, "xmax": 0.0},
 }
-
-if "R" in CRPARAMSMASTER["logParameters"]:
-    ylabel["R"] = r"R (kpc)"
+# if "R" in CRPARAMSMASTER["logParameters"]:
+#     ylabel["R"] = r"R (kpc)"
     
 for entry in CRPARAMSMASTER["logParameters"]:
     ylabel[entry] = r"$\mathrm{Log_{10}}$ " + ylabel[entry]
@@ -332,6 +329,9 @@ if __name__ == "__main__":
                     else:
                         xlimDict["R"]['xmin'] = 0.0
                         xlimDict["R"]['xmax'] = CRPARAMS['Router']
+
+                    if ("R" in CRPARAMS["logParameters"]):
+                        xlimDict["R"]['xmax'] = np.log10(xlimDict["R"]['xmax'])
 
                 except Exception as e:
 
@@ -642,14 +642,16 @@ if __name__ == "__main__":
                 if tmpCRPARAMS['analysisType'] == 'cgm':
                     xlimDict["R"]['xmin'] = 0.0#tmpCRPARAMS["Rinner"]
                     xlimDict["R"]['xmax'] = tmpCRPARAMS['Router']
-
                 elif tmpCRPARAMS['analysisType'] == 'ism':
                     xlimDict["R"]['xmin'] = 0.0
                     xlimDict["R"]['xmax'] = tmpCRPARAMS['Rinner']
                 else:
                     xlimDict["R"]['xmin'] = 0.0
                     xlimDict["R"]['xmax'] = tmpCRPARAMS['Router']
-                    
+
+                if ("R" in CRPARAMS["logParameters"]):
+                    xlimDict["R"]['xmax'] = np.log10(xlimDict["R"]['xmax'])
+
                 tmp = np.asarray(list(CRPARAMS["nonMassWeightDict"].values()))
                 whereNone = np.where(tmp==None)[0]
                 whereNOTNone = np.where(tmp!=None)[0]
@@ -661,20 +663,20 @@ if __name__ == "__main__":
                     if param not in statsWeightkeys:
                         exclusions.append(param)
 
-                if ((np.nanmax(dataDict[selectKey]["R"])<=1.0) & ("R" in CRPARAMS["logParameters"])):
-                    warnings.warn(
-                          "xParam == R has been set to log scale, but the maximum value found in the dta is <= 1 ."
-                          +"\n"
-                          +"Assuming R has been set as R/R200c (R/Rvir in code variables) which is incompatible with log scaling."
-                          +"\n"
-                          +"Will return R to kpc units before calculating further values, but please ensure this is desired and that"
-                          +" this logic has triggered correctly."
-                          )
+                # if ((np.nanmax(dataDict[selectKey]["R"])<=1.0) & ("R" in CRPARAMS["logParameters"])):
+                #     warnings.warn(
+                #           "xParam == R has been set to log scale, but the maximum value found in the dta is <= 1 ."
+                #           +"\n"
+                #           +"Assuming R has been set as R/R200c (R/Rvir in code variables) which is incompatible with log scaling."
+                #           +"\n"
+                #           +"Will return R to kpc units before calculating further values, but please ensure this is desired and that"
+                #           +" this logic has triggered correctly."
+                #           )
                 
-                    dataDict[selectKey]["R"] = (dataDict[selectKey]["R"])*dataDict[selectKey]["Rvir"]
+                #     dataDict[selectKey]["R"] = (dataDict[selectKey]["R"])*dataDict[selectKey]["Rvir"]
 
-                    xlimDict["R"]['xmin'] = 1.0
-                    xlimDict["R"]['xmax'] = xlimDict["R"]['xmax']*dataDict[selectKey]["Rvir"]
+                #     xlimDict["R"]['xmin'] = 0.0
+                #     xlimDict["R"]['xmax'] = np.log10(xlimDict["R"]['xmax']*dataDict[selectKey]["Rvir"])[0]
                     
                 print(tmpCRPARAMS['analysisType'], xlimDict["R"]['xmin'],
                     xlimDict["R"]['xmax'])
@@ -695,6 +697,9 @@ if __name__ == "__main__":
                     xlimDict=xlimDict,
                     exclusions=exclusions,
                 )
+
+                if ("R" in CRPARAMS["logParameters"]):
+                    xlimDict["R"]['xmax'] = np.log10(xlimDict["R"]['xmax'])
 
                 # # for pressureRatio in ["Pthermal_Pmagnetic","PCR_Pthermal","PCR_Pmagnetic"]:
                 # #     if pressureRatio in list(dataDict[selectKey].keys()):
@@ -717,12 +722,25 @@ if __name__ == "__main__":
 
                 if len(CRPARAMS["colParams"])>0:
 
+                    if CRPARAMS['analysisType'] == 'cgm':
+                        xlimDict["R"]['xmin'] = 0.0#tmpCRPARAMS["Rinner"]
+                        xlimDict["R"]['xmax'] = CRPARAMS['Router']
+                    elif CRPARAMS['analysisType'] == 'ism':
+                        xlimDict["R"]['xmin'] = 0.0
+                        xlimDict["R"]['xmax'] = CRPARAMS['Rinner']
+                    else:
+                        xlimDict["R"]['xmin'] = 0.0
+                        xlimDict["R"]['xmax'] = CRPARAMS['Router']
+
+                    if ("R" in CRPARAMS["logParameters"]):
+                        xlimDict["R"]['xmax'] = np.log10(xlimDict["R"]['xmax'])
+
                     # Create variant of xlimDict specifically for images of col params
                     tmpxlimDict = copy.deepcopy(xlimDict)
 
-                    # Add the col param specific limits to the xlimDict variant
-                    for key, value in colImagexlimDict.items():
-                        tmpxlimDict[key] = value
+                    # # # # # Add the col param specific limits to the xlimDict variant
+                    # # # # for key, value in colImagexlimDict.items():
+                    # # # #     tmpxlimDict[key] = value
 
                     #---------------#
                     # Check for any none-position-based parameters we need to track for col params:
@@ -785,6 +803,20 @@ if __name__ == "__main__":
                 f"[@{CRPARAMS['halo']}, @{CRPARAMS['resolution']}, @{CRPARAMS['CR_indicator']}{CRPARAMS['no-alfven_indicator']}, @{int(snapNumber)}]: Medians profile plots..."
                 )
 
+
+                if CRPARAMS['analysisType'] == 'cgm':
+                    xlimDict["R"]['xmin'] = 0.0#tmpCRPARAMS["Rinner"]
+                    xlimDict["R"]['xmax'] = CRPARAMS['Router']
+                elif CRPARAMS['analysisType'] == 'ism':
+                    xlimDict["R"]['xmin'] = 0.0
+                    xlimDict["R"]['xmax'] = CRPARAMS['Rinner']
+                else:
+                    xlimDict["R"]['xmin'] = 0.0
+                    xlimDict["R"]['xmax'] = CRPARAMS['Router']
+
+                if ("R" in CRPARAMS["logParameters"]):
+                    xlimDict["R"]['xmax'] = np.log10(xlimDict["R"]['xmax'])
+                    
                 apt.cr_medians_versus_plot(
                     tmpStatsDict,
                     CRPARAMSHALO,
@@ -822,9 +854,9 @@ if __name__ == "__main__":
                     # # # Create variant of xlimDict specifically for images of col params
                     # # tmpxlimDict = copy.deepcopy(xlimDict)
 
-                    # # # Add the col param specific limits to the xlimDict variant
-                    # # for key, value in colImagexlimDict.items():
-                    # #     tmpxlimDict[key] = value
+                    # # # # Add the col param specific limits to the xlimDict variant
+                    # # # for key, value in colImagexlimDict.items():
+                    # # #     tmpxlimDict[key] = value
 
                     #---------------#
                     # Check for any none-position-based parameters we need to track for col params:
@@ -956,11 +988,25 @@ if __name__ == "__main__":
                         )
 
                 if ((CRPARAMS["binByParam"] is True)&(len(CRPARAMS["pdfParams"])>0)):
+
+                    tmpxlimDict = copy.deepcopy(xlimDict)
+
+                    if CRPARAMS['analysisType'] == 'cgm':
+                        tmpxlimDict["R"]['xmin'] = 0.0#tmpCRPARAMS["Rinner"]
+                        tmpxlimDict["R"]['xmax'] = CRPARAMS['Router']
+
+                    elif CRPARAMS['analysisType'] == 'ism':
+                        tmpxlimDict["R"]['xmin'] = 0.0
+                        tmpxlimDict["R"]['xmax'] = CRPARAMS['Rinner']
+                    else:
+                        tmpxlimDict["R"]['xmin'] = 0.0
+                        tmpxlimDict["R"]['xmax'] = CRPARAMS['Router']
+
                     binIndices = range(0,CRPARAMS["NParamBins"]+1,1)
                     for ii,(lowerIndex,upperIndex) in enumerate(zip(binIndices[:-1],binIndices[1:])):
                         print("Starting Binned PDF plot ",ii+1," of ",CRPARAMS["NParamBins"])
                         
-                        bins = np.round(np.linspace(start=xlimDict[CRPARAMS["binParam"]]["xmin"],stop=xlimDict[CRPARAMS["binParam"]]["xmax"],num=CRPARAMS["NParamBins"]+1,endpoint=True),decimals=2)
+                        bins = np.round(np.linspace(start=tmpxlimDict[CRPARAMS["binParam"]]["xmin"],stop=tmpxlimDict[CRPARAMS["binParam"]]["xmax"],num=CRPARAMS["NParamBins"]+1,endpoint=True),decimals=2)
                         
                         whereNotInBin = ((tmpDataDict[selectKey][CRPARAMS["binParam"]]>=bins[lowerIndex])&(tmpDataDict[selectKey][CRPARAMS["binParam"]]<=bins[upperIndex]))==False
 
@@ -1047,9 +1093,9 @@ if __name__ == "__main__":
                     # Create variant of xlimDict specifically for images of col params
                     tmpxlimDict = copy.deepcopy(xlimDict)
 
-                    # Add the col param specific limits to the xlimDict variant
-                    for key, value in colImagexlimDict.items():
-                        tmpxlimDict[key] = value
+                    # # # # Add the col param specific limits to the xlimDict variant
+                    # # # for key, value in colImagexlimDict.items():
+                    # # #     tmpxlimDict[key] = value
 
                     #---------------#
                     # Check for any none-position-based parameters we need to track for col params:
@@ -1149,11 +1195,26 @@ if __name__ == "__main__":
                             )
 
                     if ((COLCRPARAMS["binByParam"] is True)&(len(COLCRPARAMS["pdfParams"])>0)):
+
+                        tmp2xlimDict = copy.deepcopy(tmpxlimDict)
+
+                        if COLCRPARAMS['analysisType'] == 'cgm':
+                            tmp2xlimDict["R"]['xmin'] = 0.0#tmpCRPARAMS["Rinner"]
+                            tmp2xlimDict["R"]['xmax'] = COLCRPARAMS['Router']
+
+                        elif COLCRPARAMS['analysisType'] == 'ism':
+                            tmp2xlimDict["R"]['xmin'] = 0.0
+                            tmp2xlimDict["R"]['xmax'] = COLCRPARAMS['Rinner']
+                        else:
+                            tmp2xlimDict["R"]['xmin'] = 0.0
+                            tmp2xlimDict["R"]['xmax'] = COLCRPARAMS['Router']
+
+
                         binIndices = range(0,COLCRPARAMS["NParamBins"]+1,1)
                         for ii,(lowerIndex,upperIndex) in enumerate(zip(binIndices[:-1],binIndices[1:])):
                             print("Starting Binned PDF plot ",ii+1," of ",COLCRPARAMS["NParamBins"])
                             
-                            bins = np.round(np.linspace(start=xlimDict[COLCRPARAMS["binParam"]]["xmin"],stop=xlimDict[COLCRPARAMS["binParam"]]["xmax"],num=COLCRPARAMS["NParamBins"]+1,endpoint=True),decimals=2)
+                            bins = np.round(np.linspace(start=tmp2xlimDict[COLCRPARAMS["binParam"]]["xmin"],stop=tmp2xlimDict[COLCRPARAMS["binParam"]]["xmax"],num=COLCRPARAMS["NParamBins"]+1,endpoint=True),decimals=2)
                             
                             whereNotInBin = ((tmpColDict[selectKeyCol][COLCRPARAMS["binParam"]]>=bins[lowerIndex])&(tmpColDict[selectKeyCol][COLCRPARAMS["binParam"]]<=bins[upperIndex]))==False
 
@@ -1208,7 +1269,7 @@ if __name__ == "__main__":
                         tmpColDict,
                         COLCRPARAMSHALO,
                         ylabel,
-                        tmpxlimDict,
+                        xlimDict,
                         snapNumber,
                         weightKeys = COLCRPARAMS['nonMassWeightDict'],
                         xParams = COLCRPARAMS["colParams"],

CR_Stage2-Plot-Time-averages_log-xParam-variant.py
@@ -18,6 +18,7 @@ import json
 import copy
 import os
 import math
+import warnings
 
 # =============================================================================#
 #
@@ -276,7 +277,22 @@ if __name__ == "__main__":
             # else:
             #     xlimDict["R"]['xmin'] = 0.0
             #     xlimDict["R"]['xmax'] = CRPARAMS['Router']/2.0
+            if ((np.nanmax(xlimDict["R"]['xmax'])<=1.0) & ("R" in CRPARAMS["logParameters"])):
+                warnings.warn(
+                        "xParam == R has been set to log scale, but the xlimDict maximum value found is <= 1 ."
+                        +"\n"
+                        +"Assuming R has been set as R/R200c (R/Rvir in code variables) which is incompatible with log scaling."
+                        +"\n"
+                        +"Will return R to kpc units before calculating further values, but please ensure this is desired and that"
+                        +" this logic has triggered correctly."
+                        )
+            
+            --- check logic for non R/rvir averaging. Code wasn't intended for such usecase, so may be averaging bins in unexpected ways
 
+                dataDict[selectKey]["R"] = (dataDict[selectKey]["R"])*dataDict[selectKey]["Rvir"]
+
+                xlimDict["R"]['xmin'] = 0.0
+                xlimDict["R"]['xmax'] = np.log10(xlimDict["R"]['xmax']*dataDict[selectKey]["Rvir"])
         
         # ----------------------------------------------------------------------#
         #  Plots...
@@ -344,7 +360,7 @@ if __name__ == "__main__":
         #     saveFigureData = False,
         #     saveFigure = CRPARAMS["SaveImages"],
         #     selectKeysList = keepPercentiles,
-        #     alignSelectKeys = "vertical",
+        #     compareSelectKeysOn = "vertical",
         #     subfigures = True,
         #     subfigureDatasetLabelsBool = True,
         #     subfigureOffAlignmentAxisLabels = False,
@@ -432,7 +448,7 @@ if __name__ == "__main__":
         #     saveFigureData = False,
         #     saveFigure = CRPARAMS["SaveImages"],
         #     selectKeysList = None,
-        #     alignSelectKeys = "vertical",
+        #     compareSelectKeysOn = "vertical",
         #     subfigures = True,
         #     subfigureDatasetLabelsBool = True,
         #     subfigureOffAlignmentAxisLabels = True,

CR_Subroutines.py
@@ -407,6 +407,11 @@ def cr_analysis_radial(
 
     whereSatellite = np.isin(snap.data["subhalo"],np.array([-1,int(CRPARAMS["HaloID"]),np.nan]))==False
 
+    print(
+        f"[@{CRPARAMS['halo']}, @{CRPARAMS['resolution']}, @{CRPARAMS['CR_indicator']}{CRPARAMS['no-alfven_indicator']}, @{int(snapNumber)}]: Remove Satellites ..."
+    )
+
+
     snap = remove_selection(
         snap,
         removalConditionMask = whereSatellite,
@@ -754,6 +759,11 @@ def cr_parameters(CRPARAMSMASTER, simDict):
     else:
         CRPARAMS["no-alfven_indicator"] = ""
 
+    try:
+        if CRPARAMS["CR_diff_coeff"] is not None:
+            CRPARAMS["CR_indicator"] += "_" + CRPARAMS["CR_diff_coeff"]
+    except:
+        pass
 
     return CRPARAMS
 
@@ -986,18 +996,14 @@ def cr_calculate_statistics(
     
     print(f"[@cr_calculate_statistics]: Excluded properties (as passed into 'exclusions' kwarg): {exclusions}")
 
+
     print("[@cr_calculate_statistics]: Generate bins")
-    if xParam in CRPARAMS["logParameters"]:
-        xBins = np.logspace(
-            start=np.log10(xlimDict[xParam]["xmin"]),
-            stop=np.log10(xlimDict[xParam]["xmax"]),
-            num=Nbins+1,
-            base=10.0,
-        )
-    else:
-        xBins = np.linspace(
-            start=xlimDict[xParam]["xmin"], stop=xlimDict[xParam]["xmax"], num=Nbins+1
-        )
+    xBins = np.linspace(
+        start=xlimDict[xParam]["xmin"], stop=xlimDict[xParam]["xmax"], num=Nbins+1
+    )
+
+    if ((xParam in CRPARAMS["logParameters"])):
+        xBins = np.power(10.0,xBins)
 
     xmin, xmax = np.nanmin(xBins), np.nanmax(xBins)
 

Plotting_tools-26-11-2023.py
@@ -1496,7 +1496,7 @@ def plot_slices(snap,
     saveFigureData = False,
     saveFigure = True,
     selectKeysList = None,
-    alignSelectKeys = "vertical",
+    compareSelectKeysOn = "vertical",
     rasterized = True,
     subfigures = False,
     cbarscale = 0.4,
@@ -1549,13 +1549,13 @@ def plot_slices(snap,
         multrows = 1
         multcols = 1
         if selectKeysList is not None:
-            if (alignSelectKeys.lower() == "vertical"):
+            if (compareSelectKeysOn.lower() == "vertical"):
                 multrows = np.shape(selectKeysList)[0]
-            elif (alignSelectKeys.lower() == "horizontal"):
+            elif (compareSelectKeysOn.lower() == "horizontal"):
                 multrows = np.shape(selectKeysList)[0]
                 multcols = np.shape(selectKeysList)[0]
             else:
-                raise Exception(f"[@plot_slices]: FAILURE! Unkown alignSelectKeys = {alignSelectKeys} requested! Please use 'vertical' or 'horizontal'! ")
+                raise Exception(f"[@plot_slices]: FAILURE! Unkown compareSelectKeysOn = {compareSelectKeysOn} requested! Please use 'vertical' or 'horizontal'! ")
 
             tmp = []
             tmpprojection = []
@@ -1587,9 +1587,9 @@ def plot_slices(snap,
                             
                             key = sliceParam[rr][cc]
                             if selectKeysList is not None:
-                                # if (alignSelectKeys.lower() == "vertical"):
+                                # if (compareSelectKeysOn.lower() == "vertical"):
                                 hasData = tmpdataSources[kk][key]
-                                # elif (alignSelectKeys.lower() == "horizontal"):
+                                # elif (compareSelectKeysOn.lower() == "horizontal"):
                                 #     hasData = tmpdataSources[ll][key]
                             else:
                                 hasData = inputDict[key]
@@ -1601,7 +1601,7 @@ def plot_slices(snap,
                             else:
                                 hasPlot = False
                         except:
-                        #     if (selectKeysList is not None) & (alignSelectKeys.lower() == "horizontal"):
+                        #     if (selectKeysList is not None) & (compareSelectKeysOn.lower() == "horizontal"):
                         #         hasPlot = False
                         #         tmp1 = copy.deepcopy(tmpsliceParam[cc])
                         #         tmp1[rr] = None
@@ -1627,7 +1627,7 @@ def plot_slices(snap,
         hasPlotMask = np.asarray(hasPlotMask)
         figshape = np.shape(hasPlotMask)
 
-        if (selectKeysList is not None) & (alignSelectKeys.lower() == "horizontal"):
+        if (selectKeysList is not None) & (compareSelectKeysOn.lower() == "horizontal"):
             sliceParam = sliceParam.T
             projection = projection.T
             hasPlotMask = hasPlotMask.T
@@ -1727,7 +1727,7 @@ def plot_slices(snap,
                 projection[subplotCount] = None
                 continue
             if (selectKeysList is not None):
-                if(alignSelectKeys.lower() == "horizontal"):
+                if(compareSelectKeysOn.lower() == "horizontal"):
                     datasourceindex = axindex[1]
                 else:
                     datasourceindex = axindex[0]
@@ -1774,7 +1774,7 @@ def plot_slices(snap,
     if subfigures:
         newxsize = xsize
         newysize = ysize
-        if (alignSelectKeys.lower() == "horizontal"):
+        if (compareSelectKeysOn.lower() == "horizontal"):
             width = float(figshape[1])+cbarscale
             height = float(figshape[0])
             aspect_ratio = width/height
@@ -1792,7 +1792,7 @@ def plot_slices(snap,
             projection = np.pad(np.asarray(projection).reshape(figshape[0],-1),((0,0),(0,1)),constant_values=None)
             projection = projection.flatten().tolist()
             hasPlotMask = np.pad(hasPlotMask,((0,0),(0,1)),constant_values=False)
-        elif (alignSelectKeys.lower() == "vertical"):
+        elif (compareSelectKeysOn.lower() == "vertical"):
             width = float(figshape[1])
             height = float(figshape[0])+cbarscale
             aspect_ratio = height/width
@@ -1810,7 +1810,7 @@ def plot_slices(snap,
             hasPlotMask = np.pad(hasPlotMask,((0,1),(0,0)),constant_values=False)
 
     if subfigures:
-        if (alignSelectKeys.lower() == "horizontal"):
+        if (compareSelectKeysOn.lower() == "horizontal"):
             fig, axes = plt.subplots(
                 nrows=figshape[0],
                 ncols=figshape[1],
@@ -1818,7 +1818,7 @@ def plot_slices(snap,
                 dpi=DPI,
                 gridspec_kw={'width_ratios': [1]*multcols + [cbarscale]}
             )
-        elif (alignSelectKeys.lower() == "vertical"):
+        elif (compareSelectKeysOn.lower() == "vertical"):
             fig, axes = plt.subplots(
                 nrows=figshape[0],
                 ncols=figshape[1],
@@ -1843,7 +1843,7 @@ def plot_slices(snap,
 
             axindex = np.unravel_index(subplotCount, shape=figshape)
             if (selectKeysList is not None):
-                if(alignSelectKeys.lower() == "horizontal"):
+                if(compareSelectKeysOn.lower() == "horizontal"):
                     datasourceindex = axindex[1]
                 else:
                     datasourceindex = axindex[0]
@@ -1852,10 +1852,10 @@ def plot_slices(snap,
                 if saveFigure:
                     currentAx = axes[axindex]
                     currentAx.axis('off')
-                    if (axindex[0]==(figshape[0]-2))&(alignSelectKeys.lower() == "vertical"): 
+                    if (axindex[0]==(figshape[0]-2))&(compareSelectKeysOn.lower() == "vertical"): 
                         cax = axes[axindex[0]+1,axindex[1]]
                         cax.axis("off")
-                    elif (axindex[1]==(figshape[1]-2))&(alignSelectKeys.lower() == "horizontal"): 
+                    elif (axindex[1]==(figshape[1]-2))&(compareSelectKeysOn.lower() == "horizontal"): 
                         cax = axes[axindex[0],axindex[1]+1]
                         cax.axis("off")
                     plotlist.append(None)
@@ -2039,7 +2039,7 @@ def plot_slices(snap,
                 currentAx.tick_params(axis="both", direction="in")
                 if subfigures == True:
                     if (selectKeysList is not None):
-                        if(alignSelectKeys.lower() == "horizontal"):
+                        if(compareSelectKeysOn.lower() == "horizontal"):
                             datasourceindex = axindex[1]
                         else:
                             datasourceindex = axindex[0]
@@ -2133,7 +2133,7 @@ def plot_slices(snap,
             # currentAx.set_xlabel(f"{AxesLabels[Axes[0]]}" + " (kpc)", fontsize=fontsize)
             # Pad snapNumber with zeroes to enable easier video making
             if ((subfigures == True)&(sharex == True)&(sharey == True)):
-                if (alignSelectKeys.lower() == "vertical"):
+                if (compareSelectKeysOn.lower() == "vertical"):
                     rowoffset = 2
                 else:
                     rowoffset = 1
@@ -2157,7 +2157,7 @@ def plot_slices(snap,
                     else:
                         currentAx.set_xticks(ticks=[])
 
-                if (axindex[0]==(figshape[0]-2))&(alignSelectKeys.lower() == "vertical"): 
+                if (axindex[0]==(figshape[0]-2))&(compareSelectKeysOn.lower() == "vertical"): 
                     caxorient = "horizontal"
                     caxtickloc = "bottom"
                     cax = axes[axindex[0]+1,axindex[1]]
@@ -2178,7 +2178,7 @@ def plot_slices(snap,
                     clb.ax.xaxis.set_major_formatter(formatter)
                     clb.ax.xaxis.set_minor_formatter(formatter) 
 
-                elif (axindex[1]==(figshape[1]-2))&(alignSelectKeys.lower() == "horizontal"): 
+                elif (axindex[1]==(figshape[1]-2))&(compareSelectKeysOn.lower() == "horizontal"): 
                     caxorient = "vertical"
                     caxtickloc = "left"
                     labellocation = "left"